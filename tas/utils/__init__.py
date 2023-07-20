b63dea660760639750404cf0852020f3b94f32ca
BlindedShooter <gkadnflh@gmail.com>
2023-07-20 20:21:53 +0900

    update utils/init.py

--- tas/utils/__init__.py
+++ tas/utils/__init__.py
@@ -1,7 +1,7 @@
 from tas.typing import ArrayLike, InfoDict, Observation, Action, Reward, Done, State, Value, ActionDist, Dynamics, ValueFuncType, CriticType, ActorType
 from tas.utils.done_funcs import DONE_FUNCS, not_done_func
 from tas.utils.torch_funcs import avg_l1_norm, shrink_and_perturb, weight_reset, gaussian_mse_loss
-from tas.utils.data import seq_to_numpy, seq_to_torch, infinite_generator, numpify, tensorify
+from tas.utils.data import seq_to_numpy, seq_to_torch, infinite_generator, numpify, tensorify, split_trajectory, stack_trajectory
 from tas.utils.d4rl_viz import visualize_traj, images_to_video
 from tas.utils.logger import Logger
 from tas.utils.env import mjenv_set_state
